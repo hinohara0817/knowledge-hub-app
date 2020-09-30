4c7b22b6c9adb00f8c73fe7b5cf5e112394061f4
hinohara <hinoshin0817@gmail.com>
2020-09-30 13:07:14 +0900

    react setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        posts: []
+    };
+
+    componentDidMount() {
+        this.getTodos();
+    }
+
+    getTodos() {
+        axios
+            .get('http://localhost:8000/api/')
+            .then(res => {
+                this.setState({ posts: res.data });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.posts.map(item => (
+                     <div key={item.id}>
+                        <h1>{item.title}</h1>
+                        <p>{item.body}</p>
+                     </div>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default App;
